8e89278aba4515c5c9f501788ae5680ed392e6c4
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2026-02-03 15:43:03 -0300

    feat: adiciona novos campos bankCode e mode ao modelo de acordos regionais e reorganiza a lista de estados para melhor legibilidade

--- scripts/extract/constants.ts
+++ scripts/extract/constants.ts
@@ -16,38 +16,68 @@ export const saveLogFilters: SaveLogFilter[] = [
   // { regional: ['BA', 'PR'] }, 
   {
     bankCode: ['001', '104'],
-    regional: ['MS', 'BA', 'PR', 'ES', 'AC', 'AL', 'AM', 'AP', 'DF', 'CE', 'ES', 'GO', 'MA', 'MG', 'MT', 'RN', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RO', 'RR', 'TO', 'CE', 'MA', 'RN', 'SE']
+    regional: [
+      'MS',
+      'BA',
+      'PR',
+      'ES',
+      'AC',
+      'AL',
+      'AM',
+      'AP',
+      'DF',
+      'CE',
+      'ES',
+      'GO',
+      'MA',
+      'MG',
+      'MT',
+      'RN',
+      'PB',
+      'PE',
+      'PI',
+      'PR',
+      'RJ',
+      'RO',
+      'RR',
+      'TO',
+      'CE',
+      'MA',
+      'RN',
+      'SE'
+    ]
   },
 ]
 
 export const agreementToRegional = [
   { agreement: '73356', regional: 'MS' },
-  { agreement: '073356', regional: 'MS' },
-  { agreement: '234757', regional: 'BA' },
-  { agreement: '081294', regional: 'PR' },
+  { agreement: '073356', regional: 'MS', bankCode: '104', mode: '4parts' }, //
+  { agreement: '234757', regional: 'BA', bankCode: '104' }, //
+  { agreement: '081294', regional: 'PR', bankCode: '104', mode: '4parts' }, //
   { agreement: '81294', regional: 'PR' },
-  { agreement: '052996', regional: 'ES' },
+  { agreement: '052996', regional: 'ES', bankCode: '104', mode: '4parts' }, //
   { agreement: '52996', regional: 'ES' },
-  { agreement: '2835965', regional: 'AC' },
-  { agreement: '2835375', regional: 'AL' },
-  { agreement: '2909128', regional: 'AM' },
-  { agreement: '2828039', regional: 'AP' },
-  { agreement: '2848659', regional: 'DF' },
-  { agreement: '3726559', regional: 'CE' },
-  { agreement: '3632840', regional: 'ES' },
-  { agreement: '2832069', regional: 'GO' },
-  { agreement: '3711056', regional: 'MA' },
-  { agreement: '2832133', regional: 'MG' },
-  { agreement: '3704138', regional: 'MT' },
-  { agreement: '3643071', regional: 'RN' },
-  { agreement: '3402948', regional: 'PB' },
-  { agreement: '2810159', regional: 'PE' },
-  { agreement: '2810627', regional: 'PI' },
-  { agreement: '3650623', regional: 'PR' },
-  { agreement: '2807857', regional: 'RJ' },
-  { agreement: '3618432', regional: 'RO' },
-  { agreement: '3556968', regional: 'RR' },
-  { agreement: '3398378', regional: 'TO' },
+  { agreement: '2835965', regional: 'AC', bankCode: '001' }, //
+  { agreement: '2835375', regional: 'AL', bankCode: '001' }, //
+  { agreement: '2909128', regional: 'AM', bankCode: '001' }, //
+  { agreement: '2828039', regional: 'AP', bankCode: '001' }, //
+  { agreement: '2848659', regional: 'DF', bankCode: '001' }, //
+  { agreement: '3726559', regional: 'CE', bankCode: '001' }, //
+  { agreement: '3632840', regional: 'ES', bankCode: '001' }, //
+  { agreement: '2832069', regional: 'GO', bankCode: '001', mode: '4parts' }, //
+  { agreement: '3711056', regional: 'MA', bankCode: '001' }, //
+  { agreement: '2832133', regional: 'MG', bankCode: '001', mode: '4parts' }, //
+  { agreement: '3704138', regional: 'MT', bankCode: '001' }, //
+  { agreement: '3643071', regional: 'RN', bankCode: '001' }, //
+  { agreement: '3402948', regional: 'PB', bankCode: '001' }, //
+  { agreement: '2810159', regional: 'PE', bankCode: '001' }, //
+  { agreement: '2810627', regional: 'PI', bankCode: '001' }, //
+  { agreement: '3650623', regional: 'PR', bankCode: '001', mode: '4parts' }, //
+  { agreement: '2807857', regional: 'RJ', bankCode: '001', mode: '4parts' }, //
+  { agreement: '3618432', regional: 'RO', bankCode: '001' }, //
+  { agreement: '3556968', regional: 'RR', bankCode: '001' }, //
+  { agreement: '3398378', regional: 'TO', bankCode: '001' }, //
+  { agreement: '3085950', regional: 'MS', bankCode: '001', mode: '4parts' },
   { agreement: '054743', regional: 'CE' },
   { agreement: '052261', regional: 'MA' },
   { agreement: '220180', regional: 'RN' },
@@ -89,7 +119,6 @@ export const agreementToRegional = [
   { agreement: '2891862', regional: '' },
   { agreement: '052358', regional: '' },
   { agreement: '051317', regional: '' },
-  { agreement: '3085950', regional: '' },
   { agreement: '054074', regional: '' },
   { agreement: '219695', regional: '' },
   { agreement: '081052', regional: '' },
